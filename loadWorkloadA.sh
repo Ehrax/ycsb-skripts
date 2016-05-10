802916355354cf9b01b6b09b8ce6ae9da3d1a84e
Ubuntu <ubuntu@localhost.localdomain>
2016-05-10 11:33:27 +0000

    blubb

--- loadWorkloadA.sh
+++ loadWorkloadA.sh
@@ -3,4 +3,4 @@ TIMESTAMP=$(date -d "today" +"%H:%M_%d-%m-%Y")
 
 cd ~/YCSB
 
-bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url="mongodb://134.60.64.243:27017/ycsb?w=1" > $HOME/loadMongodb_${TIMESTAMP}.txt 2>&1
+bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url="mongodb://192.168.0.73:27017/ycsb?w=1" > $HOME/loadMongodb_${TIMESTAMP}.txt 2>&1
