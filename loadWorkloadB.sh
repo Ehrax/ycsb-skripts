123c4928da8f331c391994571a462f1b04fa90b8
Ubuntu <ubuntu@localhost.localdomain>
2016-05-11 11:50:08 +0000

    fix

--- loadWorkloadB.sh
+++ loadWorkloadB.sh
@@ -3,4 +3,4 @@ TIMESTAMP=$(date -d "today" +"%H:%M_%d-%m-%Y")
 
 cd ~/YCSB
 
-bin/ycsb load mongodb -s -P workloads/workloadb -p mongodb.url="mongodb://192.168.0.73:27017/ycsb?w=1" > $HOME/loadMongodb_${TIMESTAMP}.txt 2>&1
+bin/ycsb load mongodb -s -P workloads/workloadb -p mongodb.url="mongodb://192.168.0.89:27017/ycsb?w=1" > $HOME/loadMongodb_${TIMESTAMP}.txt 2>&1
--- runWorkloadB.sh
+++ runWorkloadB.sh
@@ -3,4 +3,4 @@ TIMESTAMP=$(date -d "today" +"%H:%M_%d-%m-%Y")
 
 cd ~/YCSB
 
-bin/ycsb run mongodb-async -s -P workloads/workloadb -p mongodb.url="mongodb://192.168.0.73:27017/ycsb?w=1" > $HOME/runMongodb_workloadB_${TIMESTAMP}.txt 2>&1
+bin/ycsb run mongodb-async -s -P workloads/workloadb -p mongodb.url="mongodb://192.168.0.89:27017/ycsb?w=1" > $HOME/runMongodb_workloadB_${TIMESTAMP}.txt 2>&1
