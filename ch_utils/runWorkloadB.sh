63d7266bd60b08e581dffa90bfc188f790600c19
Ubuntu <ubuntu@localhost.localdomain>
2016-07-16 11:16:45 +0000

    reworked ch_utils

--- ch_utils/runWorkloadB.sh
+++ ch_utils/runWorkloadB.sh
@@ -4,5 +4,5 @@
 TIMESTAMP=$(date -d "today" +"%H:%M_%d-%m-%Y")
 
 #switch to the root directory of the YCSB
-cd ../../
+cd ~/YCSB
 bin/ycsb run couchbase2 -s -P workloads/workloadb -P couchbase2/conf/couchbase.properties > $HOME/workloadB_${TIMESTAMP}.dat 2>&1
