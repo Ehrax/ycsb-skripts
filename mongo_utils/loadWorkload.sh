10f50dae91401a8a11a615fb1ee4b56333e6587d
Alexander Rasputin <alexander.rasputin@uni-ulm.de>
2016-08-03 14:06:13 +0000

    added new ip address

--- mongo_utils/loadWorkload.sh
+++ mongo_utils/loadWorkload.sh
@@ -3,4 +3,4 @@ TIMESTAMP=$(date -d "today" +"%H:%M_%d-%m-%Y")
 
 cd ~/YCSB
 
-bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url="mongodb://192.168.0.107:27017/ycsb?w=1" > $HOME/loadMongodb_${TIMESTAMP}.txt 2>&1
+bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url="mongodb://134.60.51.220:27017/ycsb?w=1" > $HOME/loadMongodb_${TIMESTAMP}.txt 2>&1
--- mongo_utils/runWorkloadA.sh
+++ mongo_utils/runWorkloadA.sh
@@ -3,4 +3,4 @@ TIMESTAMP=$(date -d "today" +"%H:%M_%d-%m-%Y")
 
 cd ~/YCSB
 
-bin/ycsb run mongodb -s -P workloads/workloada -p mongodb.url="mongodb://192.168.0.107:27017/ycsb?w=1" > $HOME/runMongodb_workloadA_${TIMESTAMP}.txt 2>&1
+bin/ycsb run mongodb -s -P workloads/workloada -p mongodb.url="mongodb://134.60.51.220:27017/ycsb?w=1" > $HOME/runMongodb_workloadA_${TIMESTAMP}.txt 2>&1
